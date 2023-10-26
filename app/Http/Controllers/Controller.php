5c16b5a08ef7eee28a8eca9a79abc4b395ab0140
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-10-26 12:19:32 +0200

    Poner api definition fuera del controlador principal

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,10 +6,6 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(title="Time control API", version="0.0")
- */
-
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
---
+++ app/Swagger/ApiDefinition.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(title="Time control API", version="0.0")
+ */
+
+ class ApiDefinition {
+  //
+ }

cd77c5f94857e0e716cb78e764491af6786df5c8
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-10-26 12:20:55 +0200

    Definir parámetros globales para reutilizarlos

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -16,24 +16,12 @@ class LogoutController extends Controller
      *     summary="Cerrar la sesión de un usuario",
      *     description="Endpoint hacer el logout, que se encarga de desactivar y borrar el token de acceso",
      *     operationId="logoutUser",
-     *     @OA\Parameter(
-    *         name="Accept",
-    *         in="header",
-    *         required=true,
-    *         @OA\Schema(type="string", default="application/json"),
-    *         description="Header to indicate the response format, should be application/json"
-    *     ),
-    *     @OA\Parameter(
-    *         name="X-Requested-With",
-    *         in="header",
-    *         required=true,
-    *         @OA\Schema(type="string", default="XMLHttpRequest"),
-    *         description="Header to indicate the requested with parameter"
-    *     ),
+     *     @OA\Parameter(ref="#/components/parameters/acceptJsonHeader"),
+     *     @OA\Parameter(ref="#/components/parameters/requestedWith"),
      *     @OA\Response(response=200, description="Deslogueado exitosamente"),
      *     @OA\Response(response=401, description="No autorizado"),
      *     security={
-     *         {"sanctum": {}}
+     *         {"BearerAuth": {}}
      *     }
      * )
      */
---
+++ app/Swagger/ApiGlobalParameters.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Components(
+ *     @OA\Parameter(
+ *         parameter="acceptJsonHeader",
+ *         name="Accept",
+ *         in="header",
+ *         required=true,
+ *         @OA\Schema(type="string", default="application/json"),
+ *         description="Header to indicate the response format, should be application/json"
+ *     ),
+ *     @OA\Parameter(
+ *         parameter="requestedWith",
+ *         name="X-Requested-With",
+ *         in="header",
+ *         required=true,
+ *         @OA\Schema(type="string", default="XMLHttpRequest"),
+ *         description="Header to indicate the requested with parameter"
+ *     ),
+ * )
+ */
+
+ class ApiGlobalParameters {
+  //
+ }

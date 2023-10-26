e12cdce94fa828b455bb4429b48284b117d94824
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-10-26 20:25:59 +0200

    ampliar info del api

--- app/Swagger/ApiDefinition.php
+++ app/Swagger/ApiDefinition.php
@@ -4,7 +4,20 @@ namespace App\Swagger;
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Info(title="Time control API", version="0.0")
+ * @OA\Info(
+ *  title="Time control API", 
+ *  version="0.0",
+ *  description="Este API permite gestionar y contabilizar el tiempo que dedicamos a proyectos",
+ *  @OA\Contact(
+ *    name="Soporte API",
+ *    url="https://escuela.it/contacto",
+ *    email="contacto@escuela.it"
+ *  ),
+ *  @OA\License(
+ *    name="MIT",
+ *    url="https://opensource.org/license/mit/"
+ *  )
+ * )
  */
 
  class ApiDefinition {

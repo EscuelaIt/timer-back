675df83e3a9169a00c86de97e775a0aba6e8194d
midesweb <malvarez@desarrolloweb.com>
2026-05-09 11:13:56 +0200

    poder buscar por parecidos

--- app/Http/Controllers/Tag/TagListController.php
+++ app/Http/Controllers/Tag/TagListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Tag;
 
 use App\Models\Tag;
+use Illuminate\Http\Request;
 use App\Lib\ApiFeedbackSender;
 use App\Http\Controllers\Controller;
 
@@ -10,9 +11,11 @@ class TagListController extends Controller
 {
     use ApiFeedbackSender;
 
-    public function index()
+    public function index(Request $request)
     {
-        $tags = Tag::orderBy('name', 'asc')->get();
+        $tags = Tag::orderBy('name', 'asc')
+            ->when($request->filled('search'), fn ($q) => $q->where('name', 'like', "%{$request->search}%"))
+            ->get();
 
         return $this->sendSuccess('Tags', $tags);
     }
